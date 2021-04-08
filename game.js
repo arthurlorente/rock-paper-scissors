b76a9d38a46953063844a75e5ff478152a27a158
Arthur Lorente <arthurlorente@gmail.com>
2021-04-08 17:52:04 -0400

    finalized game initial structure

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-let scoreComputer = 0
-let scorePlayer = 0
+
 
 //function to determine the computer's move each round//
 
@@ -32,10 +31,61 @@ function (playerSelection) {
     return playerChoice;
 }
 
+//function to play the five rounds//
+
 function playRound(playerSelection, computerSelection) {
+    let scoreComputer = 0;
+    let scorePlayer = 0;
+    let winMessage = "The Player chose ${playerSelection} and The Computer chose ${computerSelection}. Round number ${roundNumber} was won by You! Yaaay! Computer Score: $(scoreComputer). Player Score: ${scorePlayer}.";
+    let drawMessage = "The Player chose ${playerSelection} and The Computer chose ${computerSelection}. Round number ${roundNumber} was a tie! Computer Score: ${scoreComputer}. Player Score: ${scorePlayer}.";
+    let loseMessage = "The Player chose ${playerSelection} and The Computer chose ${computerSelection}. Round number ${roundNumber} was won by The Computer! Booo! Computer Score: ${scoreComputer}. Player Score: ${scorePlayer}.";
+    function winRound() {
+        scorePlayer++;
+        console.log(winMessage);   
+    }
+    function drawRound() {
+        console.log(drawMessage);
+    }
+    function loseRound() {
+        scoreComputer++;
+        console.log(loseMessage);
+    }
     for (roundNumber = 0; roundNumber < 5; roundNumber++) {
-        if playerSelection == "rock" && computerSelection == rock {
-            console.log("Round number ${roundNumber} was a tie! Computer Score: ${scoreComputer}. Player Score: $(scorePlayer")
+        if (playerSelection == "rock" && computerSelection == "rock") {
+            drawRound();
         }
+        else if (playerSelection == "rock" && computerSelection == "paper") {
+            loseRound();
+        }
+        else if (playerSelection == "rock" && computerSelection == "scissors") {
+            winRound();
+        }
+        else if (playerSelection == "paper" && computerSelection == "rock") {
+            winRound();
+        }
+        else if (playerSelection == "paper" && computerSelection == "paper") {
+            drawRound();
+        }
+        else if (playerSelection == "paper" && computerSelection == "scissors") {
+            loseRound();
+        }
+        else if (playerSelection == "scissors" && computerSelection == "rock") {
+            loseRound();
+        }
+        else if (playerSelection == "scissors" && computerSelection == "paper") {
+            winRound();
+        }
+        else if (playerSelection == "scissors" && computerSelection == "scissors") {
+            drawRound();
+        }
+    }
+    if (scorePlayer > scoreComputer) {
+        console.log("Congrats! You won this round of 5.");
+    }
+    else if (scoreComputer > scorePlayer) {
+        console.log("Oh no! You lost this round of 5. The computer wins!");
+    }
+    else {
+        console.log("That round of 5 was a draw! Better try again.");
     }
 }
