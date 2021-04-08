b6927effe87e0f02d6915d7ffecb8b8a7c310972
Arthur Lorente <arthurlorente@gmail.com>
2021-04-08 08:56:38 -0400

    added playerSelection and playRound functions

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let scoreComputer = 0
+let scorePlayer = 0
+
 //function to determine the computer's move each round//
 
 function computerPlay() {
@@ -5,14 +8,34 @@ function computerPlay() {
     let computerChoice
 
     if (computerRandomizer <= 1) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     }
     else if (computerRandomizer > 1 && computerRandomizer <= 2) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     }
     else if (computerRandomizer > 2 && computerRandomizer <= 3) {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
 
     return computerChoice
+}
+
+//function to determine the player's move each round//
+
+function (playerSelection) {
+    let playerChoice = prompt("Rock, paper or scissors?");
+
+    while (playerChoice.toLowerCase() != "rock" || playerChoice.toLowerCase() != "paper" || playerChoice.toLowerCase() != "scissors" {
+        playerChoice = prompt("That is not rock, paper or scissors! Choose again.");
+    }
+    
+    return playerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    for (roundNumber = 0; roundNumber < 5; roundNumber++) {
+        if playerSelection == "rock" && computerSelection == rock {
+            console.log("Round number ${roundNumber} was a tie! Computer Score: ${scoreComputer}. Player Score: $(scorePlayer")
+        }
+    }
 }
